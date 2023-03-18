b43778b9f0c61d2045d49a4f8355ebffce9f1729
Ivo <hristovivo24@gmail.com>
2023-03-18 22:05:59 +0100

    added terminal clearnig

--- src/Main.java
+++ src/Main.java
@@ -43,8 +43,9 @@ public class Main {
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
             }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
         }
-
         scanner.close();
     }
 
